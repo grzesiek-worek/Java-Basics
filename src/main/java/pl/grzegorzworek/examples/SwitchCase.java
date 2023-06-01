3d723052d25261b085606f3b3c715a98535f64e7
grzesiek-worek <grzesiekw@interia.eu>
2023-06-01 15:20:27 +0200

    Java basics - add second example with switch case

--- src/main/java/pl/grzegorzworek/examples/SwitchCase.java
+++ src/main/java/pl/grzegorzworek/examples/SwitchCase.java
@@ -5,16 +5,40 @@ public class SwitchCase {
         int dayOfWeek = 2;
         String dayOfWeekString;
 
-        switch (dayOfWeek){
-            case 1: dayOfWeekString = "Monday";
+        switch (dayOfWeek) {
+            case 1:
+                dayOfWeekString = "Monday";
                 break;
-            case 2: dayOfWeekString = "Tuesday";
+            case 2:
+                dayOfWeekString = "Tuesday";
                 break;
-            case 3: dayOfWeekString = "Wednesday";
+            case 3:
+                dayOfWeekString = "Wednesday";
                 break;
-            default: dayOfWeekString = "Invalid";
+            default:
+                dayOfWeekString = "Invalid";
                 break;
         }
         System.out.println(dayOfWeekString);
+
+        String rating = "B";
+        int points;
+
+        switch (rating) {
+            case "A":
+                points = 60;
+                break;
+            case "B":
+                points = 50;
+                break;
+            case "C":
+                points = 40;
+                break;
+            default:
+                points = 1;
+                break;
+        }
+
+        System.out.println("Your score is: " + points);
     }
 }
