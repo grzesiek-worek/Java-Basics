417485930e90f6865eda3e31fb269440b14dab7f
grzesiek-worek <grzesiekw@interia.eu>
2023-06-01 23:59:50 +0200

    examples - For loop example

---
+++ src/main/java/pl/grzegorzworek/examples/ForLoop.java
@@ -0,0 +1,11 @@
+package pl.grzegorzworek.examples;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("Program end");
+     }
+}
