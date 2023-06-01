cddf26350d0c9b2a448325009871ff2bdfff56dc
grzesiek-worek <grzesiekw@interia.eu>
2023-06-01 15:09:38 +0200

    Java basics - add example with constant

---
+++ src/main/java/pl/grzegorzworek/examples/constant.java
@@ -0,0 +1,10 @@
+package pl.grzegorzworek.examples;
+
+public class constant {
+
+    public static void main(String[] args) {
+
+        final String NAME = "Grzegorz";
+        System.out.println(NAME);
+    }
+}
