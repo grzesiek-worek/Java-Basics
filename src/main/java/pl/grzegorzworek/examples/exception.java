283e92af76581cbc32f6519ec1cfb6ab72c3a547
grzesiek-worek <grzesiekw@interia.eu>
2023-06-01 15:08:25 +0200

    Java basics - add examples with exception

---
+++ src/main/java/pl/grzegorzworek/examples/exception.java
@@ -0,0 +1,37 @@
+package pl.grzegorzworek.examples;
+
+import java.sql.SQLOutput;
+
+public class exception {
+
+    public static void printMessage(String message) {
+        if (message == null) {
+            throw new IllegalArgumentException();
+        } else {
+            System.out.println(message);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            printMessage(null);
+        } catch (IllegalArgumentException e){
+            System.out.println("Please don't pass null as a method argument");
+        }
+
+        int a = 1, b = 0, c;
+        try {
+            c = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Don't divide by zero!!");
+        }
+
+        int tab[] = { 1, 2, 3, 4, 5 };
+        try {
+            System.out.println(tab[1]);
+            System.out.println(tab[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
